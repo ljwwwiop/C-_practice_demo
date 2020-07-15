8b435845a488e3b1df251621fccb2191f7683c07
ljwwwiop <1206957838@qq.com>
2020-07-15 15:27:24 +0800

    Dijkstra最短路径算法

---
+++ DataStruct/ShortestPathDijkstra/ShortestPathDijkstra/main.cpp
@@ -0,0 +1,25 @@
+#include "ShortestPathDijkstra.h"
+
+int main()
+{
+	char vexs[] = "ABCDEF";
+	AdjMatrixDirNetwork network(vexs, 6, 6);
+
+	network.InsertArc(0, 1, 45);
+	network.InsertArc(0, 2, 50);
+	network.InsertArc(0, 3, 15);
+	network.InsertArc(1, 2, 5);
+	network.InsertArc(1, 4, 20);
+	network.InsertArc(1, 5, 15);
+	network.InsertArc(3, 0, 10);
+	network.InsertArc(3, 1, 10);
+	network.InsertArc(3, 4, 79);
+	network.InsertArc(4, 1, 30);
+	network.InsertArc(5, 4, 20);
+
+	network.Display();
+	ShortestPathDijkstra(network, 0);
+	
+	system("pause");
+	return 0;
+}
